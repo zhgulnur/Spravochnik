f46a27d6f5bf5776448985a6d5066494b297ef01
Гульнур <gulnur.zhakhina@gmail.com>
2024-01-09 11:47:49 +0600

    HW

---
+++ notes.py
@@ -0,0 +1,44 @@
+######## Lecture 1
+
+# n = 5
+# print(n)
+# print(type(n))
+
+# For comments
+
+"""
+print(5)
+print(6)
+"""
+
+# Triple " quotes several lines at once
+
+# Command K + Command C -> to quote the lines
+# Command K + Command U -> to unquote the lines
+
+# f-строки:
+# a = 5
+# b = 5.89
+# c = 'vfehi'
+# print(f"{a} - {b} - {c}")
+# # или же:
+# print("{} - {} - {}".format(a,b,c))
+
+
+# Ввод данных, чтобы пользователь писал через консоль:
+# print("Enter the first number: ")
+# d = int(input())
+# e = int(input("Enter the second number: "))
+# print(d + e)
+
+# / - деление как обычно
+# % - остаток от деления
+# // - целочисленное деление
+# ** - степень
+
+# Округление:
+# f = 5.789
+# g = 8.9043
+# print(round(f*g, 3)) # пишете сколько знаков после запятой нужно
+
+
